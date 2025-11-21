7a8a30b4626cb8e06399bbf19adf1c881db994d1
Rakashi Anil Reddy <rakashianilreddy4547@gmail.com>
2025-11-21 19:42:59 +0530

    added script

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,3 +1,3 @@
-C:\Users\91954\Desktop\DataPower\APIC\APIC_CLI\apic --accept-license client-creds:set credentials.json
+C:\Users\91954\Desktop\DataPower\APIC\APIC_CLI\apic --accept-license client-creds:set C:\Users\91954\Desktop\DataPower\APIC\APIC_CLI\apic\credentials.json
 
 C:\Users\91954\Desktop\DataPower\APIC\APIC_CLI\apic --accept-license login --sso --context provider --server https://platform-api.us-east-a.apiconnect.automation.ibm.com --apiKey d50d6f78-59ed-4571-ac83-083154214d90
