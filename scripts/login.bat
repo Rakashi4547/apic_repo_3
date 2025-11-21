e60d418dcd6035a0abf3100cd1a4f005a7758c20
Rakashi Anil Reddy <rakashianilreddy4547@gmail.com>
2025-11-21 18:54:19 +0530

    added script

---
+++ scripts/login.bat
@@ -0,0 +1,2 @@
+apic client-creds:set credentials.json
+apic login --sso --context provider --server https://platform-api.us-east-a.apiconnect.automation.ibm.com --apiKey d50d6f78-59ed-4571-ac83-083154214d90
